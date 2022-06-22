29b5414b1d430ed398abac372e4efd3d8dcdabe0
Igor-Andrade-Cezar <igorhexx@gmail.com>
2022-06-22 17:16:48 -0300

    first commit

---
+++ katas1.js
@@ -0,0 +1,32 @@
+function oneThroughTwenty() {
+    let meuRetorno = [];
+    let n = 0
+    while(n< 20){
+        n++
+    meuRetorno.push(n);
+    }
+    return console.log (meuRetorno)
+}
+oneThroughTwenty()
+
+function evensToTwenty(n) {
+    let meuRetorno = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 == 0){
+            meuRetorno.push(i)
+        }
+    }
+    return meuRetorno 
+}
+console.log(evensToTwenty(20))
+
+function oddsToTwenty(n) {
+    let meuRetorno = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0){
+            meuRetorno.push(i)
+        }
+    }
+    return meuRetorno 
+}
+console.log(oddsToTwenty(20))
